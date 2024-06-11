baca2b3bd5ef99588250b1d0ce43aeb6db707a5e
Yogesh Limbani <yrlimbani03@gmail.com>
2024-06-11 21:55:04 +0530

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,21 @@
 import { NavLink } from 'react-router-dom';
 
-const navbarClass = 'text-center rounded-md p-2 hover:text-stone-200 hover:bg-stone-800 hover:outline hover:outline-2 hover:outline-cyan-300';
-const navbarClassActive = 'text-center rounded-md p-2 text-stone-200 bg-stone-800 outline outline-2 outline-cyan-300';
+const navbarClass = 'text-center rounded-md py-2 px-8 hover:text-stone-200 hover:bg-stone-800 hover:outline hover:outline-2 hover:outline-cyan-300';
+const navbarClassActive = 'text-center rounded-md py-2 px-8 text-stone-200 bg-stone-800 outline outline-2 outline-cyan-300';
 
 export default function Navbar() {
   return (
-    <aside className="w-1/12 px-1 py-8 bg-[#111111] text-stone-50">
-      <li className='px-2 py-3'><NavLink className={({ isActive }) =>
+    <aside className="w-1/12 px-1 py-8 bg-[#111111] text-stone-50 flex flex-col items-center">
+      <li className='py-3 list-none'><NavLink className={({ isActive }) =>
         isActive ? navbarClassActive : navbarClass
       } to="/">Home</NavLink></li>
-      <li className='px-2 py-3'><NavLink className={({ isActive }) =>
+      <li className='py-3 list-none'><NavLink className={({ isActive }) =>
         isActive ? navbarClassActive : navbarClass
       } to="/projects">Projects</NavLink></li>
-      <li className='px-2 py-3'><NavLink className={({ isActive }) =>
+      <li className='py-3 list-none'><NavLink className={({ isActive }) =>
         isActive ? navbarClassActive : navbarClass
       } to="/contact">Contact</NavLink></li>
-      <li className='px-2 py-3'><NavLink className={({ isActive }) =>
+      <li className='py-3 list-none'><NavLink className={({ isActive }) =>
         isActive ? navbarClassActive : navbarClass
       } to="https://drive.google.com/drive/folders/1m1ydZS4t9GOkjkcqSP8I3sJyhh8dtF2J?usp=sharing" target='_blank'>Resume</NavLink></li>
     </aside>
--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -4,7 +4,7 @@ export default function ProjectItem() {
             <div
                 className="relative h-fit mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
                 <img
-                    src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80"
+                    src="/projects/moviez.jpg"
                     alt="card-image" />
             </div>
             <div className="p-6">
